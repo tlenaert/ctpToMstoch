9c1649f75b7238fbec5b27b5ba2a36450810d2af
Tom Lenaerts <tlenaert@ulb.ac.be>
2023-11-30 13:52:58 +0100

    added all the scripts and txt files for the manuscript figures

---
+++ results/Fig3/plotcompexpl4.R
@@ -0,0 +1,46 @@
+library(magrittr)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+
+#read all the files
+flist<-list.files(pattern="ctpstepsexpl4tris.txt")
+dt_list <- lapply(flist, read.delim, header = FALSE)
+
+#prepare data for plotting
+data<-data.frame(dt_list)
+names(data)<- c("beta", "eps","S0", "S1", "S2", "S3", "S4")
+data<-data[,-1]
+data<-data[,-1]
+mkpavg <- c(0.071, 0.356, 0.370, 0.153, 0.049)
+
+
+data<-rbind(data, mkpavg)
+
+newdata <-c(1,2,3,4,5)
+newdata <-rbind(newdata,data)
+
+newdata<-t(newdata)
+newdata<-data.frame(newdata)
+names(newdata)<-c("steps", "Present model", "Experiment")
+df <- melt(newdata, id.vars="steps")
+
+plot<-ggplot(data=df, aes(x=steps, y=value, fill=variable)) +
+  geom_bar(stat="identity", color="black", position=position_dodge())+
+  labs(x="Steps", y = "Frequency", fill="")+
+  scale_fill_manual(values=c("#0072B2","#FF6600"))+
+  scale_x_continuous(breaks=0:6)+
+  theme_minimal()+
+  expand_limits(y=0.4)+
+  theme(legend.position = c(0.8, 0.8),
+        legend.text = element_text(size=27),
+        legend.title = element_text(size=27),
+        axis.title.x=element_text(size=35),
+        axis.title.y=element_text(size=35),
+        panel.border = element_blank(),
+        axis.line = element_line(colour="black", size=1),
+        axis.text.x = element_text(size=27),
+        axis.text.y=element_text(size=27))
+plot
+
---
+++ results/Fig3/plotlevcontour.R
@@ -0,0 +1,58 @@
+library(magrittr)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+library(metR)
+
+
+#read all the files
+flist<-list.files(pattern="ctperrorLallfine.txt")
+dt_list <- lapply(flist, read.delim, header = FALSE)
+
+#prepare data for plotting
+data.cont<-data.frame(dt_list)
+names(data.cont)<- c("beta", "eps", "mse","rmse","avgb","avgl","avgd","diff","avgf")
+min(data.cont$rmse)
+min(data.cont$mse)
+
+#make smooth contour
+mod <- loess(avgl ~ beta + eps, data = data.cont,span=0.2)
+df <- with(data.cont, 
+           expand.grid(eps = seq(min(eps), max(eps), len = 100),
+                       beta = seq(min(beta), max(beta), len = 100)))
+
+df$yvar <- c(predict(mod, newdata = df, se = FALSE))
+
+min(data.cont$avgl)
+max(data.cont$avgl)
+
+#plot contour and bets fit
+myplot <-ggplot(df, aes(x=beta, y=eps, z=yvar))+
+  geom_raster(aes(fill = yvar))+
+  geom_contour(color="white",bins=20)+
+  scale_fill_viridis_c(limits = c(0.9,2.6), breaks = c(1.0, 1.5, 2.0, 2.5)) +
+  guides(fill = guide_colourbar(barheight=20,nbin=100)) +
+  geom_vline(xintercept = 0.31, colour="red", linetype="dashed")+
+  geom_hline(yintercept = 0.19, colour="red", linetype="dashed")+
+  geom_point(x=0.31, y=0.19, size=6, colour="red")+
+  annotate("text", x=0.315, y=0.20, label= "("~beta~"=0.31,"~epsilon~"=0.19)", colour="white", size=7, hjust="left")+
+  geom_text_contour(aes(z = yvar),stroke = 0.2, size =7)+
+  xlab(beta~"(selection strength)")+
+  ylab(epsilon~"(reasoning error probability)")+
+  labs(fill='Average level')+ 
+  #coord_fixed()+
+  theme_minimal()+
+  theme(
+        #legend.position = "none",
+        legend.text = element_text(size=18),
+        legend.title = element_text(size=27),
+        axis.title.x=element_text(size=35),
+        axis.title.y=element_text(size=35),
+        panel.border = element_blank(),
+        axis.line = element_line(colour="black", size=1),
+        axis.text.x = element_text(size=27),
+        axis.text.y=element_text(size=27))
+myplot
+
+
